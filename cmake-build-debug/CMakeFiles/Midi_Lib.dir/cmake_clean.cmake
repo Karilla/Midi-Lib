e9b2b4cac7e5e1cc39a2c928ae4c035a554c2988
Karilla <bdelay155@gmail.com>
2021-10-09 18:41:59 +0200

    First commit

---
+++ cmake-build-debug/CMakeFiles/Midi_Lib.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Midi_Lib.dir/src/main.cpp.obj"
+  "Midi_Lib.exe"
+  "Midi_Lib.exe.manifest"
+  "Midi_Lib.pdb"
+  "libMidi_Lib.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Midi_Lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
